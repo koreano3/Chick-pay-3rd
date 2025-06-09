e94c333310265db1be6552fd46e2d13607c7308d
arronso <bajasung@gmail.com>
2025-06-09 14:14:59 +0900

    테라폼파일 최신화

--- infra/terraform/eks/service/main.tf
+++ infra/terraform/eks/service/main.tf
@@ -12,10 +12,11 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "ap-northeast-2"
+  profile = "eomsigi"
 }
 
-# VPC 모듈에서 생성한 VPC와 서브넷 정보 가져오기
+# VPC 정보 불러오기
 data "terraform_remote_state" "vpc" {
   backend = "local"
   config = {
@@ -23,7 +24,7 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-# 서비스용 EKS 클러스터 생성
+# EKS 클러스터 생성
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -31,14 +32,14 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  cluster_endpoint_public_access  = false
+  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
   vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
 
   enable_irsa = true
-  enable_cluster_creator_admin_permissions = true  # 생성자에게 system:masters 권한 부여
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     service-nodes = {
@@ -55,9 +56,9 @@ module "eks" {
   }
 }
 
-# EKS 클러스터 정보 및 인증 토큰 가져오기
+# 클러스터 정보 불러오기
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
@@ -65,9 +66,14 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
-# Kubernetes provider 설정
+# kubernetes provider 설정 (try()로 에러 방지 + alias)
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  token                  = data.aws_eks_cluster_auth.this.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-} 
+  alias                  = "eks"
+  host                   = try(data.aws_eks_cluster.this.endpoint, "https://dummy")
+  token                  = try(data.aws_eks_cluster_auth.this.token, "dummy-token")
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "dummy-ca")
+}
+
+resource "null_resource" "wait_for_eks" {
+  depends_on = [module.eks]
+}
