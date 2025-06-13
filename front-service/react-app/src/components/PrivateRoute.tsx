113fc723e225fc45684cc2c95e0252d8535d96fa
arronso <bajasung@gmail.com>
2025-06-14 00:59:11 +0900

    Recover react-app folder as normal directory

--- front-service/react-app/src/components/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React, { JSX } from "react";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const isAuthenticated = !!localStorage.getItem("access_token");
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
